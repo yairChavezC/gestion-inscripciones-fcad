d5e21ff7ba9a02468e5b42d204aa6c7c409f5b01
Antobella5 <150458181+Antobella5@users.noreply.github.com>
2026-05-20 18:49:21 -0300

    cambié solo el cursoscontroller

--- backend/src/routes/cursosroutes.js
+++ backend/src/routes/cursosroutes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 // Agregamos eliminarCurso a la importación
-const { getCursos, getCursoId,crearCurso, actualizarCurso, eliminarCurso } = require('../controllers/cursosController');
+const { getCursos, getCursoId,crearCurso, actualizarCurso, eliminarCurso } = require('../controllers/cursoscontroller');
 
 router.get('/', getCursos);
 router.get('/:id', getCursoId); //También nueva ruta
